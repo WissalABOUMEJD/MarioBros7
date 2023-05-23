03e59ddcd65cc5a81d58a6042a426a71968fc021
Wissal Aboumejd <89734216+coqui-llage@users.noreply.github.com>
2023-05-23 22:51:47 +0200

    Update Tuyau.java

--- Mariobros7/src/pack/jeu/Tuyau.java
+++ Mariobros7/src/pack/jeu/Tuyau.java
@@ -6,17 +6,13 @@ import javax.swing.ImageIcon;
 
 public class Tuyau extends Objet{
 	
-	private ImageIcon iconTuyau;
-	private static Image imageTuyau;
 	
 	private static boolean afficher;
 	
 	public Tuyau(int x, int y) {
 		super(90, 60, x, y);
-		iconTuyau = new ImageIcon(getClass().getResource("/images/tuyau.png"));
-		imageTuyau = iconTuyau.getImage();
-		this.setImageIcon(iconTuyau);
-		this.setImageObjet(imageTuyau);
+		super.icoObjet = new ImageIcon(getClass().getResource("/images/tuyau.png"));
+		super.imageObjet = this.icoObjet.getImage();
 		afficher = true;
 	}
 	
@@ -25,21 +21,6 @@ public class Tuyau extends Objet{
 
 	}
 
-	public ImageIcon getIconBrique() {
-		return iconTuyau;
-	}
-
-	public void setIconBrique(ImageIcon iconBrique) {
-		this.iconTuyau = iconBrique;
-	}
-
-	public Image getImageBrique() {
-		return imageTuyau;
-	}
-
-	public static void setImageBrique(Image imageBrique) {
-		Tuyau.imageTuyau = imageBrique;
-	}
 
 	public static boolean isAfficher() {
 		return afficher;
