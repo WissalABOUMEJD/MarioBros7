1874d12ee5c260853b4b119612c5b3be39f44924
Wissal Aboumejd <89734216+coqui-llage@users.noreply.github.com>
2023-05-23 22:52:08 +0200

    Update Brique.java

--- Mariobros7/src/pack/jeu/Brique.java
+++ Mariobros7/src/pack/jeu/Brique.java
@@ -7,17 +7,13 @@ import javax.swing.ImageIcon;
 public class Brique extends Objet{
 	
 	private static final Collision Bas = null;
-	private ImageIcon iconBrique;
-	private static Image imageBrique;
 	
 	private static boolean afficher;
 	
 	public Brique(int x, int y) {
 		super(40, 40, x, y);
-		iconBrique = new ImageIcon(getClass().getResource("/images/Brique.png"));
-		imageBrique = iconBrique.getImage();
-		this.setImageIcon(iconBrique);
-		this.setImageObjet(imageBrique);
+		super.icoObjet = new ImageIcon(getClass().getResource("/images/Brique.png"));
+		super.imageObjet = this.icoObjet.getImage();
 		afficher = true;
 	}
 	
@@ -28,22 +24,6 @@ public class Brique extends Objet{
 		}	
 	}
 	
-
-	public ImageIcon getIconBrique() {
-		return iconBrique;
-	}
-
-	public void setIconBrique(ImageIcon iconBrique) {
-		this.iconBrique = iconBrique;
-	}
-
-	public Image getImageBrique() {
-		return imageBrique;
-	}
-
-	public static void setImageBrique(Image imageBrique) {
-		Brique.imageBrique = imageBrique;
-	}
 
 	public static boolean isAfficher() {
 		return afficher;
