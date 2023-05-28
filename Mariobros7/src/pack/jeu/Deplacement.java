7251b98f7812648b83ed9daba47f44053d8e25b4
Wissal Aboumejd <89734216+coqui-llage@users.noreply.github.com>
2023-05-28 20:06:43 +0200

    Update Deplacement.java

--- Mariobros7/src/pack/jeu/Deplacement.java
+++ Mariobros7/src/pack/jeu/Deplacement.java
@@ -6,10 +6,8 @@ import java.awt.event.KeyListener;
 import menu.Menu;
 
 public class Deplacement implements KeyListener {
-
+	public static boolean paused;
 	public boolean descendre,sauter,right,left;  // comme mario ne peut pas se baisser, on ajoute un boolean pour savoir si l'on doit descendre dans un tuyau
-	
-
 	
 	public final static int vitesseDeplacement = 4;   //Attention ce nombre doit diviser 1644.
 	
@@ -17,7 +15,11 @@ public class Deplacement implements KeyListener {
 	@Override
     public void keyPressed(KeyEvent e) {
         int keyCode = e.getKeyCode();
-        if (keyCode == KeyEvent.VK_LEFT) {
+        if (keyCode == KeyEvent.VK_ENTER) {
+            Menu.niveauPanel.isPaused = !Menu.niveauPanel.isPaused; // Inverser l'état de la variable "paused"
+            Menu.niveauPanel.temps.isPausedTemps = !Menu.niveauPanel.temps.isPausedTemps;
+        }
+        if (keyCode == KeyEvent.VK_LEFT && !Menu.niveauPanel.isPaused) {
         	left = true;
             //Menu.niveauPanel.setX(-vitesseDeplacement);   
         	if (Menu.niveauPanel.player.marcheGaucheEnCours == false) {
@@ -27,7 +29,7 @@ public class Deplacement implements KeyListener {
         	Menu.niveauPanel.player.marcheDroiteEnCours = false;
         	
         }  
-        if (keyCode == KeyEvent.VK_RIGHT) {
+        if (keyCode == KeyEvent.VK_RIGHT && !Menu.niveauPanel.isPaused) {
         	if (Menu.niveauPanel.player.marcheDroiteEnCours == false) {
         		Menu.niveauPanel.player.marcheDroiteEnCours = true;
         	}
@@ -37,7 +39,7 @@ public class Deplacement implements KeyListener {
         	//Menu.niveauPanel.setX(vitesseDeplacement);
         	right = true;
         }
-        if (keyCode == KeyEvent.VK_UP) {
+        if (keyCode == KeyEvent.VK_UP && !Menu.niveauPanel.isPaused) {
         	if (Menu.niveauPanel.player.sautEnCours == false) {
         		Menu.niveauPanel.player.sautEnCours = true;
         	}
@@ -45,7 +47,7 @@ public class Deplacement implements KeyListener {
         	Menu.niveauPanel.player.marcheDroiteEnCours = false;
         	sauter = true;
         }
-        if (keyCode == KeyEvent.VK_DOWN) {
+        if (keyCode == KeyEvent.VK_DOWN && !Menu.niveauPanel.isPaused) {
             descendre = true;
         }
     }
