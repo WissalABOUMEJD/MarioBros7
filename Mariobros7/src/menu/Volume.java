62ed881965d2d6078a4cc0ba7d8b8f150c360a0a
Wissal Aboumejd <89734216+coqui-llage@users.noreply.github.com>
2023-05-30 01:23:54 +0200

    Update Volume.java

--- Mariobros7/src/menu/Volume.java
+++ Mariobros7/src/menu/Volume.java
@@ -15,22 +15,27 @@ import javax.swing.JSlider;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
+/**
+ * Le panneau de contrôle du volume.
+ */
+public class Volume extends JPanel {
+    private JButton retourVolumeButton;  // Bouton de retour
+    private JSlider volumeSlider;  // Curseur de volume
+    private JLabel jLabel10;  // Étiquette pour l'image de fond
 
-public class Volume extends JPanel{
-	private JButton retourVolumeButton;
-	private JSlider volumeSlider;
-	private JLabel jLabel10;
-    
+    /**
+     * Construit un nouveau panneau de contrôle du volume.
+     */
     public Volume() {
-    	retourVolumeButton = new JButton();
-    	volumeSlider = new JSlider();
-    	jLabel10 = new JLabel();
-    	setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        retourVolumeButton = new JButton();
+        volumeSlider = new JSlider();
+        jLabel10 = new JLabel();
+        setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        Menu.setButton(retourVolumeButton, "Retour");
+        Menu.setButton(retourVolumeButton, "Retour");  // Configure le bouton de retour
         retourVolumeButton.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent evt) {
-                Menu.retourVolumeButtonMouseClicked(evt);
+                Menu.retourVolumeButtonMouseClicked(evt);  // Gère l'événement de clic sur le bouton de retour
             }
         });
         add(retourVolumeButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(1350, 10, 190, 30));
@@ -38,27 +43,39 @@ public class Volume extends JPanel{
         setForeground(new Color(0, 204, 0));
         add(volumeSlider, new org.netbeans.lib.awtextra.AbsoluteConstraints(640, 400, 330, 40));
 
-        jLabel10.setIcon(new ImageIcon(getClass().getResource("/images/FondProvisoire1.jpg"))); 
+        jLabel10.setIcon(new ImageIcon(getClass().getResource("/images/FondProvisoire1.jpg")));
         add(jLabel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1650, 830));
-        
+
         volumeSlider.addChangeListener((ChangeListener) new ChangeListener() {
             public void stateChanged(ChangeEvent e) {
-                updateVolume();
+                updateVolume();  // Met à jour le volume lorsque la valeur du curseur change
             }
-        });  
+        });
     }
 
+    /**
+     * Renvoie le curseur de volume.
+     * 
+     * @return le curseur de volume
+     */
     public JSlider getvolumeSlider() {
-        return volumeSlider;
-    }
-    
-    private void updateVolume() {
-        int volume = volumeSlider.getValue();
-        System.out.println("Nouvelle valeur du volume : " + volume);
+        return volumeSlider;  // Renvoie le curseur de volume
     }
 
-    
+    /**
+     * Met à jour le volume en fonction de la valeur actuelle du curseur.
+     */
+    public void updateVolume() {
+        int volume = volumeSlider.getValue();
+        System.out.println("Nouvelle valeur du volume : " + volume);  // Affiche la nouvelle valeur du volume dans la console
+    }
+
+    /**
+     * Renvoie le bouton de retour.
+     * 
+     * @return le bouton de retour
+     */
     public JButton getRetourButton() {
-        return retourVolumeButton;
+        return retourVolumeButton;  // Renvoie le bouton de retour
     }
 }
