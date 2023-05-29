b1b6d9563fb80ca34c7702700d26c59ea60a8e79
Wissal Aboumejd <89734216+coqui-llage@users.noreply.github.com>
2023-05-30 01:21:53 +0200

    Update Langues.java

--- Mariobros7/src/menu/Langues.java
+++ Mariobros7/src/menu/Langues.java
@@ -1,67 +1,81 @@
 package menu;
 
-import java.awt.Color;
-import java.awt.Font;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.Dimension;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
+
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class Langues extends JPanel{
-	private JButton englishButton;
-    private JButton frenchButton;
-    private JButton retourLangueButton;
-    private JLabel jLabel9;
-    
+public class Langues extends JPanel {
+    private JButton englishButton;  // Bouton pour sélectionner la langue anglaise
+    private JButton frenchButton;  // Bouton pour sélectionner la langue française
+    private JButton retourLangueButton;  // Bouton pour revenir au menu des options
+    private JLabel jLabel9;  // Étiquette pour l'image de fond
+
+    /**
+     * Construit un nouveau panneau pour la sélection de la langue.
+     */
     public Langues() {
-    	englishButton = new JButton();
-    	frenchButton = new JButton();
-    	retourLangueButton = new JButton();
-    	jLabel9 = new JLabel();
+        englishButton = new JButton();
+        frenchButton = new JButton();
+        retourLangueButton = new JButton();
+        jLabel9 = new JLabel();
 
-    	setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        Menu.setButton(frenchButton, "Français");
+        Menu.setButton(frenchButton, "Français");  // Configure le bouton pour la langue française
         frenchButton.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent evt) {
-                Menu.frenchButtonMouseClicked(evt);
+                Menu.frenchButtonMouseClicked(evt);  // Gère l'événement de clic sur le bouton pour la langue française
             }
         });
         add(frenchButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(710, 350, -1, 50));
 
-        Menu.setButton(retourLangueButton, "Retour");
+        Menu.setButton(retourLangueButton, "Retour");  // Configure le bouton de retour
         retourLangueButton.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent evt) {
-                Menu.retourLangueButtonMouseClicked(evt);
+                Menu.retourLangueButtonMouseClicked(evt);  // Gère l'événement de clic sur le bouton de retour
             }
         });
         add(retourLangueButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(1350, 10, 190, 30));
 
-        Menu.setButton(englishButton, "Anglais");
+        Menu.setButton(englishButton, "Anglais");  // Configure le bouton pour la langue anglaise
         englishButton.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent evt) {
-                Menu.englishButtonMouseClicked(evt);
+                Menu.englishButtonMouseClicked(evt);  // Gère l'événement de clic sur le bouton pour la langue anglaise
             }
         });
         add(englishButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(710, 430, -1, 50));
 
-        jLabel9.setIcon(new ImageIcon(getClass().getResource("/images/FondProvisoire1.jpg")));
+        jLabel9.setIcon(new ImageIcon(getClass().getResource("/images/FondProvisoire1.jpg")));  // Affiche l'image de fond
         add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1650, 840));
     }
 
-    public JButton getfrenchButton() {
+    /**
+     * Renvoie le bouton pour la langue française.
+     *
+     * @return le bouton pour la langue française
+     */
+    public JButton getFrenchButton() {
         return frenchButton;
     }
 
-    public JButton getenglishButton() {
+    /**
+     * Renvoie le bouton pour la langue anglaise.
+     *
+     * @return le bouton pour la langue anglaise
+     */
+    public JButton getEnglishButton() {
         return englishButton;
     }
     
+    /**
+     * Renvoie le bouton de retour.
+     *
+     * @return le bouton de retour
+     */
     public JButton getRetourButton() {
         return retourLangueButton;
     }
