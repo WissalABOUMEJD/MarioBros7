c20b98ca6bb0feba51194f6d3ba0f568359c20d4
Wissal Aboumejd <89734216+coqui-llage@users.noreply.github.com>
2023-05-30 01:23:07 +0200

    Update Options.java

--- Mariobros7/src/menu/Options.java
+++ Mariobros7/src/menu/Options.java
@@ -1,4 +1,5 @@
 package menu;
+
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.MouseAdapter;
@@ -12,55 +13,73 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Options extends JPanel {
-	private JLabel jLabel8;
-    private JButton volumeButton;
-    private JButton langueButton;
-    private JButton retourOptionsButton;
+    private JLabel jLabel8;  // Étiquette pour l'image de fond
+    private JButton volumeButton;  // Bouton pour accéder aux options de volume
+    private JButton langueButton;  // Bouton pour accéder aux options de langue
+    private JButton retourOptionsButton;  // Bouton de retour
 
+    /**
+     * Construit un nouveau panneau des options.
+     */
     public Options() {
-    	jLabel8 = new JLabel();
-    	volumeButton = new JButton();
-    	langueButton = new JButton();
-    	retourOptionsButton = new JButton();
-    	
-    	setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        jLabel8 = new JLabel();
+        volumeButton = new JButton();
+        langueButton = new JButton();
+        retourOptionsButton = new JButton();
+        
+        setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        Menu.setButton(langueButton, "Langue");
+        Menu.setButton(langueButton, "Langue");  // Configure le bouton de langue
         langueButton.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent evt) {
-                Menu.langueButtonMouseClicked(evt);
+                Menu.langueButtonMouseClicked(evt);  // Gère l'événement de clic sur le bouton de langue
             }
         });
         add(langueButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(710, 350, -1, 50));
 
-        Menu.setButton(volumeButton, "Volume");
+        Menu.setButton(volumeButton, "Volume");  // Configure le bouton de volume
         volumeButton.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent evt) {
-                Menu.volumeButtonMouseClicked(evt);
+                Menu.volumeButtonMouseClicked(evt);  // Gère l'événement de clic sur le bouton de volume
             }
         });
         add(volumeButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(710, 430, -1, 50));
 
-        Menu.setButton(retourOptionsButton, "Retour");
+        Menu.setButton(retourOptionsButton, "Retour");  // Configure le bouton de retour
         retourOptionsButton.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent evt) {
-                Menu.retourOptionsButtonMouseClicked(evt);
+                Menu.retourOptionsButtonMouseClicked(evt);  // Gère l'événement de clic sur le bouton de retour
             }
         });
         add(retourOptionsButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(1350, 10, 190, 30));
 
-        jLabel8.setIcon(new ImageIcon(getClass().getResource("/images/FondProvisoire1.jpg"))); // NOI18N
+        jLabel8.setIcon(new ImageIcon(getClass().getResource("/images/FondProvisoire1.jpg"))); // Affiche l'image de fond
         add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1650, 830));
     }
     
+    /**
+     * Renvoie le bouton de volume.
+     *
+     * @return le bouton de volume
+     */
     public JButton getVolumeButton() {
         return volumeButton;
     }
 
+    /**
+     * Renvoie le bouton de langue.
+     *
+     * @return le bouton de langue
+     */
     public JButton getLangueButton() {
         return langueButton;
     }
     
+    /**
+     * Renvoie le bouton de retour.
+     *
+     * @return le bouton de retour
+     */
     public JButton getRetourOptionsButton() {
         return retourOptionsButton;
     }
