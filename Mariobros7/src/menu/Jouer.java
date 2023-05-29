c196d486669b11b7b572091ce04902b4166a017f
Wissal Aboumejd <89734216+coqui-llage@users.noreply.github.com>
2023-05-30 01:21:37 +0200

    Update Jouer.java

--- Mariobros7/src/menu/Jouer.java
+++ Mariobros7/src/menu/Jouer.java
@@ -9,22 +9,25 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class Jouer extends JPanel{
-	private JLabel jLabel6;
-	private JLabel jLabel7;
-	private JLabel avatar1;
-	private JLabel avatar2;
-	private JLabel avatar3;
-	private JButton retourAvatarButton;
-	
-	public Jouer() {
-		jLabel6 = new JLabel();
-		jLabel7 = new JLabel();
-		avatar1 = new JLabel();
-		avatar2 = new JLabel();
-		avatar3 = new JLabel();
-		retourAvatarButton = new JButton();
-		
+public class Jouer extends JPanel {
+    private JLabel jLabel6;  // Étiquette pour le texte "Choisis ton avatar :"
+    private JLabel jLabel7;  // Étiquette pour l'image de fond
+    private JLabel avatar1;  // Étiquette pour l'avatar 1
+    private JLabel avatar2;  // Étiquette pour l'avatar 2
+    private JLabel avatar3;  // Étiquette pour l'avatar 3
+    private JButton retourAvatarButton;  // Bouton pour revenir au menu des options
+
+    /**
+     * Construit un nouveau panneau pour la sélection de l'avatar.
+     */
+    public Jouer() {
+        jLabel6 = new JLabel();
+        jLabel7 = new JLabel();
+        avatar1 = new JLabel();
+        avatar2 = new JLabel();
+        avatar3 = new JLabel();
+        retourAvatarButton = new JButton();
+
         setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLabel6.setFont(new Font("Press Start 2P", 1, 18));
@@ -36,7 +39,7 @@ public class Jouer extends JPanel{
         
         avatar1.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                Menu.avatar1MouseClicked(evt);
+                Menu.avatar1MouseClicked(evt);  // Gère l'événement de clic sur l'avatar 1
             }
         });
 
@@ -45,7 +48,7 @@ public class Jouer extends JPanel{
         
         avatar2.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                Menu.avatar2MouseClicked(evt);
+                Menu.avatar2MouseClicked(evt);  // Gère l'événement de clic sur l'avatar 2
             }
         });
 
@@ -54,40 +57,64 @@ public class Jouer extends JPanel{
         
         avatar3.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                Menu.avatar3MouseClicked(evt);
+                Menu.avatar3MouseClicked(evt);  // Gère l'événement de clic sur l'avatar 3
             }
         });
 
-        Menu.setButton(retourAvatarButton, "Retour");
+        Menu.setButton(retourAvatarButton, "Retour");  // Configure le bouton de retour
         retourAvatarButton.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent evt) {
-                Menu.retourAvatarButtonMouseClicked(evt);
+                Menu.retourAvatarButtonMouseClicked(evt);  // Gère l'événement de clic sur le bouton de retour
             }
         });
         add(retourAvatarButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(1350, 10, 190, 30));
 
-        jLabel7.setIcon(new ImageIcon(getClass().getResource("/images/FondProvisoire1.jpg")));
+        jLabel7.setIcon(new ImageIcon(getClass().getResource("/images/FondProvisoire1.jpg")));  // Affiche l'image de fond
         add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1650, 830));
-	}
-	
-	public JButton getRetourButton() {
-		return retourAvatarButton;
-	}
-	
-	public JLabel getAvatar1() {
-		return avatar1;
-	}
-	
-	public JLabel getAvatar2() {
-		return avatar2;
-	}
-	
-	public JLabel getAvatar3() {
-		return avatar3;
-	}
-	
-	public JLabel getJlabel() {
-		return jLabel6;
-	}
+    }
 
+    /**
+     * Renvoie le bouton de retour.
+     *
+     * @return le bouton de retour
+     */
+    public JButton getRetourButton() {
+        return retourAvatarButton;
+    }
+    
+    /**
+     * Renvoie l'étiquette pour l'avatar 1.
+     *
+     * @return l'étiquette pour l'avatar 1
+     */
+    public JLabel getAvatar1() {
+        return avatar1;
+    }
+    
+    /**
+     * Renvoie l'étiquette pour l'avatar 2.
+     *
+     * @return l'étiquette pour l'avatar 2
+     */
+    public JLabel getAvatar2() {
+        return avatar2;
+    }
+    
+    /**
+     * Renvoie l'étiquette pour l'avatar 3.
+     *
+     * @return l'étiquette pour l'avatar 3
+     */
+    public JLabel getAvatar3() {
+        return avatar3;
+    }
+    
+    /**
+     * Renvoie l'étiquette pour le texte "Choisis ton avatar :".
+     *
+     * @return l'étiquette pour le texte "Choisis ton avatar :"
+     */
+    public JLabel getJLabel() {
+        return jLabel6;
+    }
 }
